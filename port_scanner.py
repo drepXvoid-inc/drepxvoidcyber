b65ad2d2dcf6d4942686fbd679088a573af92ae3
drepXvoid-inc <voidxdrep@gmail.com>
2026-02-13 23:35:03 +0700

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,16 +1,20 @@
 import socket
 
-target = "127.0.0.1"
+target = input("Masukkan IP target: ")
+start_port = int(input("Start port: "))
+end_port = int(input("End port: "))
 
-print("Scanning localhost...")
+print(f"\nScanning {target} dari port {start_port} sampai {end_port}...\n")
 
-for port in range(1, 200):
+for port in range(start_port, end_port + 1):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(0.3)
-    
+
     result = s.connect_ex((target, port))
-    
+
     if result == 0:
-        print(f"Port {port} terbuka")
-    
+        print(f"[OPEN] Port {port}")
+
     s.close()
+
+print("\nScan selesai.")
